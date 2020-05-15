e7eae363f04a3d256aea752ccdc643a087698606
Shadip Banik <shadipbanik000@gmail.com>
2020-05-15 23:43:58 +0600

    first commit

---
+++ product/read.php
@@ -0,0 +1,17 @@
+<?php
+// required headers
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: application/json; charset=UTF-8");
+// include database and object files
+include_once '../config/database.php';
+include_once '../objects/product.php';
+  
+// instantiate database and product object
+$database = new Database();
+$db = $database->getConnection();
+  
+// initialize object
+$product = new Product($db);
+  
+// read products will be here
+?>
